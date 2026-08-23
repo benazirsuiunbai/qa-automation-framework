164eda42f925481ffd6b5abee27c3911e86dc893
Benazir Suiunbai <benazirsuiunbai@icloud.com>
2026-08-22 22:36:04 -0500

    Add first Selenium PyTest automation test

---
+++ tests/test_google.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+def test_google_title():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.google.com")
+
+    assert "Google" in driver.title
+
+    driver.quit()
